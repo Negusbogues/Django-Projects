f354f1c061a57aca9c689c3c0a2afc39604e1c15
NegusBogues <bogues.n@live.com>
2023-01-05 02:25:26 -0500

    1/5

--- portfolio/tabs/models.py
+++ portfolio/tabs/models.py
@@ -1,3 +1,5 @@
+import random
+from string import ascii_letters
 from django.db import models
 
 # Create your models here.
@@ -18,3 +20,30 @@ class WeightConvert(models.Model):
         l = 0.45359237
         res = round(self.Weight*l,2)            
         return res
+class PasswordGen(models.Model):
+    Length = models.IntegerField()
+    Special = models.BooleanField()
+    def make_password(self):
+        code = ''
+        passcode = []
+        SpecialChar = ["!","@","#","$","%","^","&","*","(",")","-","?"]
+        if self.Special == True:
+            for i in range(int(self.Length)):
+                if len(passcode) < (self.Length/3):
+                    passcode.insert(random.randint(0, self.Length), random.choice(ascii_letters))
+                elif len(passcode) < ((self.Length/3)*2):
+                    passcode.insert(random.randint(0, self.Length), (random.randint(0,10)))
+                else:
+                    passcode.insert(random.randint(0, self.Length), random.choice(SpecialChar))
+            for i in passcode:
+                code = code + str(i)
+        else:
+            for i in range(int(self.Length)):
+                if len(passcode) < (self.Length/2):
+                    passcode.insert(random.randint(0, self.Length), random.choice(ascii_letters))
+                else:
+                    passcode.insert(random.randint(0, self.Length), (random.randint(0,10)))
+            for i in passcode:
+                code = code + str(i)
+
+        return code
--- portfolio/tabs/views.py
+++ portfolio/tabs/views.py
@@ -13,18 +13,29 @@ def lbs2kgs(Weight):
     res = round(Weight*k,2)            
     return res
 def projects_view(request):
-    all = models.WeightConvert.objects.all().delete()
     if request.POST:
-        Weight = request.POST['Weight']
-        Unit = request.POST['Unit']
-        models.WeightConvert.objects.create(Weight=Weight, Unit=Unit)
-        weightinfoKG= models.WeightConvert.objects.filter(Unit= 'KG')
-        weightinfoLB= models.WeightConvert.objects.filter(Unit= 'LB')
-        context = {
-        "weightKG":weightinfoKG,
-        "weightLB":weightinfoLB,
-    }
-        return render(request, 'tabs/projects.html', context)
+        try:
+            all = models.WeightConvert.objects.all().delete()
+            Weight = request.POST['Weight']
+            Unit = request.POST['Unit']
+            models.WeightConvert.objects.create(Weight=Weight, Unit=Unit)
+            weightinfoKG= models.WeightConvert.objects.filter(Unit= 'KG')
+            weightinfoLB= models.WeightConvert.objects.filter(Unit= 'LB')       
+            context = {
+            "weightKG":weightinfoKG,
+            "weightLB":weightinfoLB,
+            }
+            return render(request, 'tabs/projects.html', context)
+        except:
+            all1 = models.PasswordGen.objects.all().delete()
+            Length = request.POST['Length']
+            Special = request.POST['Special']
+            models.PasswordGen.objects.create(Length=Length, Special=Special)
+            Pass = models.PasswordGen.objects.all()
+            context = {
+            "Pass":Pass,
+            }
+            return render(request, 'tabs/projects.html', context)
     else:
         return render(request, 'tabs/projects.html')
     
