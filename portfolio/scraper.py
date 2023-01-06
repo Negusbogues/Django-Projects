9c71c1297ede3cf81e7de443c2ec405f8e651957
NegusBogues <bogues.n@live.com>
2023-01-05 20:54:28 -0500

    1/5

---
+++ portfolio/scraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+
+#converted_gold_price=price[0:5]
+def gold_price():
+    url = 'https://www.monex.com/gold-prices/111'
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54'}
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    title = soup.find(id='productTitle')
+    gold_price = soup.find('span', id="span-813-5").get_text()
+    currentDate = datetime.date.today()
+    currentMonthName = currentDate.strftime("%B")
+    return (f"{currentMonthName} {currentDate.day}, {currentDate.year} : ") + gold_price
+
+print(gold_price())
--- portfolio/tabs/views.py
+++ portfolio/tabs/views.py
@@ -3,7 +3,7 @@ from django.http.response import HttpResponse
 from django.views.generic import TemplateView, FormView, CreateView
 from django.urls import reverse_lazy, reverse
 from . import models
-
+from scraper import gold_price
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -13,35 +13,37 @@ def lbs2kgs(Weight):
     res = round(Weight*k,2)            
     return res
 def projects_view(request):
-    if request.POST:
-        try:
-            all = models.WeightConvert.objects.all().delete()
-            Weight = request.POST['Weight']
-            Unit = request.POST['Unit']
-            models.WeightConvert.objects.create(Weight=Weight, Unit=Unit)
-            weightinfoKG= models.WeightConvert.objects.filter(Unit= 'KG')
-            weightinfoLB= models.WeightConvert.objects.filter(Unit= 'LB')       
-            context = {
+    all = models.WeightConvert.objects.all().delete()
+    all1 = models.PasswordGen.objects.all().delete()
+    weightinfoKG= models.WeightConvert.objects.filter(Unit= 'KG')
+    weightinfoLB= models.WeightConvert.objects.filter(Unit= 'LB') 
+    Pass = models.PasswordGen.objects.all()
+    context = {
             "weightKG":weightinfoKG,
             "weightLB":weightinfoLB,
+            "goldprice": gold_price(),
+            "Pass":Pass,
             }
+    if request.POST:
+        try:
+            Weight = request.POST['Weight']
+            Unit = request.POST['Unit']
+            models.WeightConvert.objects.create(Weight=Weight, Unit=Unit)    
             return render(request, 'tabs/projects.html', context)
         except:
-            all1 = models.PasswordGen.objects.all().delete()
             Length = request.POST['Length']
             Special = request.POST['Special']
             models.PasswordGen.objects.create(Length=Length, Special=Special)
-            Pass = models.PasswordGen.objects.all()
-            context = {
-            "Pass":Pass,
-            }
             return render(request, 'tabs/projects.html', context)
     else:
-        return render(request, 'tabs/projects.html')
+        return render(request, 'tabs/projects.html', context)
     
 
 def contact_view(request):
-    return render(request, 'tabs/contact.html')
+    context = {
+        'goldprice': gold_price(),
+    }
+    return render(request, 'tabs/contact.html', context)
 def result_view(request):
     weightinfoKG= models.WeightConvert.objects.filter(Unit= 'KG')
     weightinfoLB= models.WeightConvert.objects.filter(Unit= 'LB')
