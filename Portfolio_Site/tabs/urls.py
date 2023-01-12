4fa0218f086d0149069e3ebc05a9f80584446987
NegusBogues <bogues.n@live.com>
2023-01-12 00:20:22 -0500

    1/12

---
+++ Portfolio_Site/tabs/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+app_name = 'tabs'
+urlpatterns = [
+    path('',views.HomeView.as_view(), name = 'home'),
+    path('projects/',views.projects_view, name = 'projects'),
+    path('contact/',views.contact_view, name = 'contact'),
+    path('resume/',views.resume_view, name = 'resume'),
+    path('certifications/',views.certifications_view, name = 'certifications'),
+    path('thank_you/',views.thank_you_view, name = 'thank_you'),
+]
