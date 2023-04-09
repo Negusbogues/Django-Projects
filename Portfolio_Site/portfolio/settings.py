bc999847dd49e909c885ec26f5c4383fe7236843
negus <bogues.n@live.com>
2023-04-09 00:22:21 -0400

    4/9

--- Portfolio_Site/portfolio/settings.py
+++ Portfolio_Site/portfolio/settings.py
@@ -24,9 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j6#1ui$a*rgj^2h6a8b&o8=@#+!7*o3y_5siy5&#w5qv&r!64y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['44.204.9.26']
+DEBUG = False
+ALLOWED_HOSTS = ['44.204.9.26','l']
 
 
 # Application definition
